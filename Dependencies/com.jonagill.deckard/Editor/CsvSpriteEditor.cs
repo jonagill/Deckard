38ded2618f9aa37b22f92c92436b9b889183edc8
Jon Gill <jon@recroom.com>
2020-06-10 22:22:16 -0700

    Clean up example content for public release

--- Dependencies/com.jonagill.deckard/Editor/CsvSpriteEditor.cs
+++ Dependencies/com.jonagill.deckard/Editor/CsvSpriteEditor.cs
@@ -7,20 +7,5 @@ namespace Deckard.Editor
     public class CsvSpriteEditor : CsvDataBehaviourEditor
     {
         private CsvSprite Target => (CsvSprite) target;
-
-        private SerializedProperty spriteCollectionProperty;
-
-        private void OnEnable()
-        {
-            spriteCollectionProperty = serializedObject.FindProperty("spriteCollection");
-        }
-
-        public override void OnInspectorGUI()
-        {
-            EditorGUILayout.PropertyField(spriteCollectionProperty);
-            EditorGUILayout.Separator();
-            
-            base.OnInspectorGUI();
-        }
     }
 }
