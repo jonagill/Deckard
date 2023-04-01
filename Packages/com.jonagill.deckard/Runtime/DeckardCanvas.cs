2b86b10a604455a12d136f4b5df650c3bf491daf
Jon Gill <jonagill@gmail.com>
2023-03-31 21:14:45 -0700

    Fix issue where card backs using sprites would be the wrong size when using the default card size

--- Packages/com.jonagill.deckard/Runtime/DeckardCanvas.cs
+++ Packages/com.jonagill.deckard/Runtime/DeckardCanvas.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
@@ -159,6 +155,12 @@ namespace Deckard
                     EnforceCorrectConfiguration();
                 }
             }
+        }
+
+        private void Awake()
+        {
+            // Snap the RectTransform to its current size
+            EnforceCorrectConfiguration();
         }
 
         private void Start()
